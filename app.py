3ebf350277596654c9e07421e4a2cfa16dd5b7a4
MiqSA <eng.miqueias.sa@gmail.com>
2020-12-05 01:12:58 -0300

    Add register page

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 # config import
 from config import app_config, app_active
 
-
 config = app_config[app_active]
+
 
 def create_app(config_name):
     app = Flask(__name__, template_folder='templates')
@@ -15,8 +16,19 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
+    Bootstrap(app)
+
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/register/')
+    def register():
+        return render_template('register.html')
+
+    @app.route('/register/', methods=['POST'])
+    def register_post():
+        number = int(request.form['number'])
+        return render_template('register.html', number=number)
 
     return app
